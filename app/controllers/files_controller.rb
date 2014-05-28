16e5d42554acab9b3321cd5c21861fc3bfee89bd
Alex Frost <alex.frost.dev@gmail.com>
2014-05-28 20:56:44 +0100

    use models in controller

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,7 +1,8 @@
 class FilesController < ApplicationController
   def show
     if cookies[:workshare]
-      ApiConnection.new.get_files(cookies[:workshare])
+      api_files = ApiConnection.new.get_files(cookies[:workshare])
+      @weight_calculator = WeightCalculator.new(api_files.body['files'])
     else
       redirect_to new_session_path
     end
