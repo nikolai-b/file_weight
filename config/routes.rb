942e8faf508c385978f57b703a07143c2f866680
Alex Frost <alex.frost.dev@gmail.com>
2014-05-28 13:39:00 +0100

    add files and sessions controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root 'files#show'
+
+  get 'files/show'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
