9b942297dcecaa9f36140b8e405e8ca6a39f7b82
Alex Frost <alex.frost.dev@gmail.com>
2014-05-28 14:28:53 +0100

    use user model to pass user data around

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.new (set_user_params)
+    cookies[:workshare] = ApiConnection.get_authentication_cookie(user)
+  end
+
+  private
+
+  def set_user_params
+    user_params = params.require(:user)
+
+    {
+      email: user_params[:email],
+      password: user_params[:password],
+      api_uuid: user_params[:api_uuid]
+    }
   end
 end
