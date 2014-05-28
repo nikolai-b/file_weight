399fe2a1dad1f63b50c72e03439ba127039710f1
Alex Frost <alex.frost.dev@gmail.com>
2014-05-28 15:03:21 +0100

    add POST spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -5,14 +5,18 @@ describe SessionsController do
   describe "GET 'new'" do
     it "returns http success" do
       get 'new'
-      response.should be_success
+      expect(response).to be_success
     end
   end
 
   describe "GET 'create'" do
+    let(:user_params) {{email: env_email, password: env_password, api_uuid: env_api_uuid}}
+    let(:user) { User.new(user_params) }
+
     it "returns http success" do
-      get 'create'
-      response.should be_success
+      expect_any_instance_of(ApiConnection).to receive(:get_authentication_cookie).with(user).and_return(true)
+
+      post :create, user_params
     end
   end
 
