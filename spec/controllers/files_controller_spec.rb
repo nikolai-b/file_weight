6aabec36ed623a2c4e75716295d8e760f476503e
Alex Frost <alex.frost.dev@gmail.com>
2014-05-28 14:26:04 +0100

    update specs to test without cookies

--- spec/controllers/files_controller_spec.rb
+++ spec/controllers/files_controller_spec.rb
@@ -3,9 +3,22 @@ require 'spec_helper'
 describe FilesController do
 
   describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
+    subject { get 'show' }
+
+    context "without cookie" do
+      it "redirects" do
+        subject.should redirect_to "/sessions/new"
+      end
+    end
+
+    context "with cookie" do
+      before do
+        cookies[:workshare] = true
+      end
+
+      it "returns http success" do
+        subject.should be_success
+      end
     end
   end
 
